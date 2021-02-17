2e84dce91c6c6705c7fd5875846daef1fe301243
Gates-Zeng <39002348+Gates-Zeng@users.noreply.github.com>
2021-02-18 01:41:32 +1100

    Update __init__.py

--- pygns3/__init__.py
+++ pygns3/__init__.py
@@ -1,5 +1,5 @@
-from .controller import GNS3API, GNS3Compute, GNS3Controller, GNS3Project, GNS3VM
+from .controller import *
 from requests.auth import HTTPBasicAuth
 
-__all__ = ['GNS3API', 'GNS3Compute', 'GNS3Controller', 'GNS3Project', 'GNS3VM']
+__all__ = ['GNS3API', 'GNS3Compute', 'GNS3Controller', 'GNS3Project', 'GNS3VM', 'GNS3Drawing', 'GNS3Image', 'GNS3Link', 'GNS3Node', 'GNS3NodePort', 'GNS3NodeProperties', 'GNS3Snapshot', 'GNS3VMEngine']
 
